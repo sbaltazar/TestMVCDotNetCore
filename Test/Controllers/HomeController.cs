c92e651be044517a08fb896c44c8471681268700
Sebastian Baltazar <sbaltazar7455@gmail.com>
2020-03-08 22:56:48 -0300

    [Add] Project creation

---
+++ Test/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Test.Models;
+
+namespace Test.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IEmployeeRepository _employeeRepository;
+
+        public HomeController(IEmployeeRepository employeeRepository)
+        {
+            _employeeRepository = employeeRepository;
+        }
+
+        public string Index()
+        { 
+            return _employeeRepository.GetEmployee(1).Name;
+        }
+
+        public ViewResult Details()
+        {
+            Employee model = _employeeRepository.GetEmployee(1);
+            ViewBag.Employee = model;
+            ViewBag.PageTitle = "Employee Details";
+            return View();
+        }
+    }
+}
---
+++ Test/Models/MockEmployeeRepository.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Test.Models
+{
+    public class MockEmployeeRepository : IEmployeeRepository
+    {
+        private List<Employee> _employeeList;
+
+        public MockEmployeeRepository()
+        {
+            _employeeList = new List<Employee>()
+            {
+                new Employee() { Id = 1, Name = "Mary", Department = "HR", Email = "mary@morro.cl" },
+                new Employee() { Id = 2, Name = "Tom", Department = "IT", Email = "tom@morro.cl" },
+                new Employee() { Id = 3, Name = "Sam", Department = "IT", Email = "sam@morro.cl" }
+            };
+        }
+
+        public Employee GetEmployee(int Id)
+        {
+            return _employeeList.FirstOrDefault(e => e.Id == Id);
+        }
+    }
+}
