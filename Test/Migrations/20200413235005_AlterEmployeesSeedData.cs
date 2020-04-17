8429008f6ffaa327440be293f1c41915a4383056
Sebastian Baltazar <sbaltazar7455@gmail.com>
2020-04-16 21:21:08 -0400

    [Add] Created data migration to SQL database using EF Core

---
+++ Test/Migrations/20200413235005_AlterEmployeesSeedData.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Test.Migrations
+{
+    public partial class AlterEmployeesSeedData : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Employees",
+                keyColumn: "Id",
+                keyValue: 1,
+                columns: new[] { "Email", "Name" },
+                values: new object[] { "marco@yupi.cl", "Marco" });
+
+            migrationBuilder.InsertData(
+                table: "Employees",
+                columns: new[] { "Id", "Department", "Email", "Name" },
+                values: new object[] { 2, 1, "rob@yupi.cl", "Rob" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Employees",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.UpdateData(
+                table: "Employees",
+                keyColumn: "Id",
+                keyValue: 1,
+                columns: new[] { "Email", "Name" },
+                values: new object[] { "elcorreo@pulento.cl", "Mark" });
+        }
+    }
+}
---
+++ Test/Models/ModelBuilderExtensions.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Test.Models
+{
+    public static class ModelBuilderExtensions
+    {
+        public static void Seed(this ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Employee>().HasData(
+                new Employee
+                {
+                    Id = 1,
+                    Name = "Marco",
+                    Department = Dept.IT,
+                    Email = "marco@yupi.cl"
+                },
+                new Employee
+                {
+                    Id = 2,
+                    Name = "Rob",
+                    Department = Dept.HR,
+                    Email = "rob@yupi.cl"
+                }
+            );
+        }
+    }
+}
