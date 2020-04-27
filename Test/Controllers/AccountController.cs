75ad1faec51d03b8101e5144b714b9fb08d91179
Sebastian Baltazar <sbaltazar7455@gmail.com>
2020-04-26 21:21:54 -0400

    [Update] Prevented open redirection attacks

--- Test/Controllers/AccountController.cs
+++ Test/Controllers/AccountController.cs
@@ -77,7 +77,7 @@ namespace Test.Controllers
 
                 if (result.Succeeded)
                 {
-                    if (!string.IsNullOrEmpty(returnUrl))
+                    if (!string.IsNullOrEmpty(returnUrl) && Url.IsLocalUrl(returnUrl))
                     {
                         return Redirect(returnUrl);
                     }
