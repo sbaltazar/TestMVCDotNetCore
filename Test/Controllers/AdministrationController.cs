cb6ff262716ee9ba74017a3cf3d8e1c010592040
Sebastian Baltazar <sbaltazar7455@gmail.com>
2020-05-03 23:24:01 -0400

    [Update] Added authorization to administration views

--- Test/Controllers/AdministrationController.cs
+++ Test/Controllers/AdministrationController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -10,6 +11,7 @@ using Test.ViewModels;
 
 namespace Test.Controllers
 {
+    [Authorize(Roles = "Admin")]
     public class AdministrationController : Controller
     {
         private readonly RoleManager<IdentityRole> roleManager;
