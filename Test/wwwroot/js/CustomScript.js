46b5e1c933a9bbc5752a2f666b509a122b8e2dfc
Sebastian Baltazar <sbaltazar7455@gmail.com>
2020-05-05 19:25:50 -0400

    [Update] Added delete confirmation

--- Test/wwwroot/js/CustomScript.js
+++ Test/wwwroot/js/CustomScript.js
@@ -1,1 +1,11 @@
-﻿
+﻿function confirmDelete(uniqueId, isDeleteClicked) {
+
+    if (isDeleteClicked) {
+        $(`#deleteSpan_${uniqueId}`).hide();
+        $(`#confirmDeleteSpan_${uniqueId}`).show();
+    } else {
+        $(`#deleteSpan_${uniqueId}`).show();
+        $(`#confirmDeleteSpan_${uniqueId}`).hide();
+    }
+
+}
