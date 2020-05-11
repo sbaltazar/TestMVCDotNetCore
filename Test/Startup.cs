f37a64928c2cf711eb6fab61714034fc11f9d4ec
Sebastian Baltazar <sbaltazar7455@gmail.com>
2020-05-11 16:37:44 -0400

    [Update] Changed token lifespan

--- Test/Startup.cs
+++ Test/Startup.cs
@@ -8,6 +8,7 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using System;
 using Test.Models;
 using Test.Security;
 
@@ -37,6 +38,8 @@ namespace Test
             })
             .AddEntityFrameworkStores<AppDbContext>()
             .AddDefaultTokenProviders();
+
+            services.Configure<DataProtectionTokenProviderOptions>(options => options.TokenLifespan = TimeSpan.FromHours(5));
 
             services.AddMvc(options =>
             {
